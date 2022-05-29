71b66173fe3d1842d8db1436d8357efa9fb25bd1
jrvldam <jrvldam@gmail.com>
2022-05-29 12:09:39 +0200

    vim tree maps

--- lua/keys.lua
+++ lua/keys.lua
@@ -10,13 +10,20 @@ keymap('n', '<C-Tab>', ':bn<CR>', options) -- Next buffer
 keymap('n', '<C-S-Tab>', ':bp<CR>', options) -- Previous buffer
 keymap('n', '<Leader>bb', ':b#<CR>', options) -- Back to last edited buffer
 
+-- Files tree
 keymap('n', '<Leader>b', ':NvimTreeToggle<CR>', options)
+keymap('n', '<Leader>n', ':NvimTreeFindFile<CR>', options)
+keymap('n', '<Leader>r', ':NvimTreeRefresh<CR>', options)
+
+-- Terminal
 keymap('n', '<Leader>y', ':FloatermToggle<CR>', options)
 
+-- Search files
 keymap('n', '<Leader>ff', ':Telescope find_files<CR>', options)
 keymap('n', '<Leader>fg', ':Telescope live_grep<CR>', options)
 keymap('n', '<Leader>fb', ':Telescope buffers<CR>', options)
 
+-- Spell
 keymap('n', '<Leader>Zg', 'Zg', {}) -- Add to spell file
 keymap('n', '<Leader>ZG', 'ZG', {}) -- Add to internal
 keymap('n', '<Leader>Zc', 'Zc', {}) -- Correct
